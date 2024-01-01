08273d4ec560a288ab52b8aa2899569ada16804c
JethroNatividad <natividad.jet@gmail.com>
2024-01-02 02:26:12 +0800

    write tests

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,27 @@
 // Process: investment_amount = principal_amount * (1 + (interest_rate_percentage * years_invested))
 // Output: After {years_invested} years at {interest_rate_percentage}%, the investment will be worth ${investment_amount}.
 
-fn calculate_interest(principal_amount, interest_rate_percentage, years_invested) -> f64 {
-    principal_amount * (1 + (interest_rate_percentage * years_invested))
+fn calculate_simple_interest(principal_amount: f64, interest_rate_percentage: f64, years_invested: f64) -> f64 {
+    let interest_rate: f64 = interest_rate_percentage / 100.0;
+    principal_amount * (1.0 + (interest_rate * years_invested))
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    #[test]
+    fn test_calculate_simple_interest(){
+        assert_eq!(calculate_simple_interest(1500.0, 4.3, 4.0), 1758.0);
+        assert_eq!(calculate_simple_interest(2000.0, 0.0, 3.0), 2000.0);
+        assert_eq!(calculate_simple_interest(0.0, 4.5, 5.0), 0.0);
+        assert_eq!(calculate_simple_interest(5000.0, 7.25, 6.0), 7175.0);
+        assert_eq!(calculate_simple_interest(100.0, 1.0, 0.5), 100.5);
+        assert_eq!(calculate_simple_interest(3000.0, -2.0, 4.0), 3000.0);
+        assert_eq!(calculate_simple_interest(2500.0, 3.75, -2.0), 2500.0);
+
+    }
+}
 
 
 fn main() {
